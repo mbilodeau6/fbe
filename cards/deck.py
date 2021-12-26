ec58fc72583314b3f69c92ef14de0faa27390ab8
Michael <mbilodeau6@hotmail.com>
2021-12-25 18:05:20 -0700

    Add right cards to deck

--- cards/deck.py
+++ cards/deck.py
@@ -1,6 +1,7 @@
 from card import Card
 from rank import Rank
 from suit import Suit
+from enum import Enum
 
 class Deck:
     def __init__(self, includeJokers):
@@ -10,5 +11,9 @@ class Deck:
             self.cards.append(Card(Suit.heart, Rank.joker))
             self.cards.append(Card(Suit.spade, Rank.joker))
 
-        for i in range(52):
-            self.cards.append(Card(Suit.heart, Rank.joker))
+        for rank in Rank:
+            if rank == Rank.joker:
+                continue
+            
+            for suit in Suit:
+                self.cards.append(Card(suit, rank))
---
+++ cards/test_deck.py
@@ -0,0 +1,45 @@
+from deck import Deck
+from card import Card
+from suit import Suit
+from rank import Rank
+from enum import Enum
+
+def validate_cards(new_deck, joker_count):
+    rank_counts = {}
+    suit_counts = {}
+
+    for rank in Rank:
+        rank_counts[rank] = 0
+
+    for suit in Suit:
+        suit_counts[suit] = 0
+
+    for card in new_deck.cards:
+        rank_counts[card.rank] += 1
+        suit_counts[card.suit] += 1
+
+    for rank in rank_counts:
+        if rank == Rank.joker:
+            assert rank_counts[rank] == joker_count, f"unexpected count for {rank}"
+        else:
+            assert rank_counts[rank] == 4, f"unexpected count for {rank}"
+
+    for suit in suit_counts:
+        expected_count = 13
+
+        if (suit == Suit.heart or suit == Suit.spade) and joker_count > 0:
+            expected_count = 14
+
+        assert suit_counts[suit] == expected_count, f"unexpected count for {suit}"
+
+def test_deck_with_jokers():
+    new_deck = Deck(True)
+    assert len(new_deck.cards) == 54
+
+    validate_cards(new_deck, 2)
+
+def test_deck_without_jokers():
+    new_deck = Deck(False)
+    assert len(new_deck.cards) == 52
+
+    validate_cards(new_deck, 0)
