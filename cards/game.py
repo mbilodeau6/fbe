d22d32cf280e001812db0459525c651359a50a13
Michael <mbilodeau6@hotmail.com>
2021-12-27 14:42:08 -0700

    Add discard card logic

--- cards/game.py
+++ cards/game.py
@@ -1,6 +1,7 @@
 from deck import Deck
 from player import Player
 from frustration_rules import FrustrationRules
+from random import randrange, seed
 
 class Game:
     def initialize_players(self, player_names):
@@ -13,7 +14,9 @@ class Game:
         if len(player_names) < 2:
             raise ValueError("Require a minimum of 2 players")
 
+        self.players_in_order = player_names[:]
         self.initialize_players(player_names)
+        self.top_of_discard = None
 
         self.deck = Deck(True)
         self.deck.shuffle()
@@ -23,6 +26,10 @@ class Game:
             for player in self.players.values():
                 player.hand.append(self.deck.get_next_card())
 
+        self.top_of_discard = self.deck.get_next_card()
+
+        self.player_turn = randrange(0, len(self.players))
+
     def display_game(self):
         for player in self.players.values():
             print(f"Name: {player.name}; Score: {player.score}")
@@ -31,3 +38,26 @@ class Game:
             for card in player.hand:
                 print(f"{repr(card)}", end="; ")
             print()
+
+        print()
+
+        # TODO: There should be a game_state to determine state
+        if self.top_of_discard is not None:
+            print(f"Top of Discard: {self.top_of_discard}")
+            print(f"Player Turn: {self.players_in_order[self.player_turn]}")
+
+    # TODO: Should use player_guid instead of name to identify player
+    # TODO: Should verify the player has the card they are discarding
+    def discard_card(self, player_name, card_to_discard):
+        if player_name != self.whose_turn_is_it():
+           raise ValueError(f"It is not {player_name}'s turn.")
+
+        self.top_of_discard = card_to_discard
+        self.players[player_name].hand.remove(card_to_discard)
+
+    # TODO: Improve logic for calling the method at the wrong time
+    def whose_turn_is_it(self):
+        if self.player_turn is None:
+            return None
+
+        return self.players_in_order[self.player_turn]
--- cards/test_game.py
+++ cards/test_game.py
@@ -2,9 +2,12 @@ from _pytest.python_api import raises
 import pytest
 from game import Game
 
-def test_initialize_2_players():
+def create_2_person_game():
     player_names = ["Michael", "Jason"]
-    new_game = Game(player_names)
+    return Game(player_names)
+
+def test_initialize_2_players():
+    new_game = create_2_person_game()
     assert len(new_game.players) == 2
     assert "Michael" in new_game.players
     assert "Jason" in new_game.players
@@ -18,3 +21,45 @@ def test_initialize_1_player():
     player_names = ["Michael"]
     with pytest.raises(ValueError):
         new_game = Game(player_names)
+
+def test_discard_card():
+    new_game = create_2_person_game()
+    assert new_game.top_of_discard is None
+
+    new_game.start_game()
+
+    player_name = new_game.whose_turn_is_it()
+    player_card_count = len(new_game.players[player_name].hand)
+    card_to_discard = new_game.players[player_name].hand[0]
+    new_game.discard_card(player_name, card_to_discard)
+    assert new_game.top_of_discard == card_to_discard
+    assert len(new_game.players[player_name].hand) == player_card_count - 1
+
+def get_other_player_name(player_name):
+    other_player = "Michael"
+    if player_name == other_player:
+        other_player = "Lisa"
+
+    return other_player
+
+def test_wrong_player_discards():
+    new_game = create_2_person_game()
+    new_game.start_game()
+
+    player_name = new_game.whose_turn_is_it()
+    other_player = get_other_player_name(player_name)
+
+    card_to_discard = new_game.players[other_player].hand[0]
+    with pytest.raises(ValueError):
+        new_game.discard_card(other_player, card_to_discard)
+
+def test_discard_wrong_card():
+    new_game = create_2_person_game()
+    new_game.start_game()
+
+    player_name = new_game.whose_turn_is_it()
+    other_player = get_other_player_name(player_name)
+
+    card_to_discard = new_game.players[other_player].hand[0]
+    with pytest.raises(ValueError):
+        new_game.discard_card(player_name, card_to_discard)
